55dce6995a5916570590e71bfef698d9c3e97f9d
Ort Moodle developer <MoodleDev@ort.org.il>
2016-11-14 15:09:11 +0200

    question/type/matrix (new)

---
+++ question/type/matrix/version.php
@@ -0,0 +1,8 @@
+<?PHP
+/**
+ * Question of type matrix
+ */
+$plugin->component = 'qtype_matrix';
+$plugin->requires  = 2013040500;
+$plugin->maturity  = MATURITY_BETA;
+$plugin->version = 2015070100;
